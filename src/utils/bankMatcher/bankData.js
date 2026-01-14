4b0eaae1d7fc3c23dd1e07971441a7125f0969ce
Jieun <jeeneep@naver.com>
2026-01-14 12:18:59 +0900

    feat: 계좌번호 검증 로직 모듈화 및 정밀도 개선

---
+++ src/utils/bankMatcher/bankData.js
@@ -0,0 +1,63 @@
+export const BANK_PLUGINS_DATA = [
+  {
+    name: "우리은행",
+    codes: ["020", "084"],
+    subjects: ["006", "05", "01", "007", "15", "10", "002", "004", "08", "18", "92", "09"],
+    rules: [
+      { length: 13, subStart: 1, subLen: 3 }, // 단축코드(1)-과목(3)
+      { length: 14, subStart: 10, subLen: 2 }, // 점(3)-고객(6)-검증(1)-과목(2)
+      { length: 11, subStart: 3, subLen: 3 }, // 점(3)-과목(3)
+      { length: 12, subStart: 5, subLen: 2 }  // 점(3)-고객(2)-과목(2)
+    ]
+  },
+  {
+    name: "신한은행",
+    codes: ["088", "021", "026"],
+    subjects: ["100", "110", "150", "140", "01", "04", "05", "81", "901", "99"],
+    rules: [
+      { length: 12, subStart: 0, subLen: 3 }, // 과목(3)-일련(8)-검증(1)
+      { length: 11, subStart: 3, subLen: 2 }, // 점(3)-과목(2)
+      { length: 13, subStart: 3, subLen: 3 }, // 가상
+      { length: 14, subStart: 0, subLen: 3 }  // 가상
+    ]
+  },
+  {
+    name: "국민은행",
+    codes: ["004", "006"],
+    subjects: ["01", "21", "24", "05", "04", "25", "26", "92"],
+    rules: [
+      { length: 12, subStart: 4, subLen: 2 }, // 점(4)-과목(2)
+      { length: 14, subStart: 4, subLen: 2 }  // 차세대/가상
+    ]
+  },
+  {
+    name: "기업은행",
+    codes: ["003", "043"],
+    subjects: ["01", "02", "03", "13", "07", "06", "04", "003"],
+    rules: [
+      { length: 14, subStart: 9, subLen: 2 }, // 점(3)-고객(6)-과목(2) 
+      { length: 11, subStart: 0, subLen: 3 }, // 11자리 (대표코드 003으로 시작하는 경우)
+      { length: 10, subStart: 0, subLen: 2 }
+    ]
+  },
+  {
+    name: "농협",
+    codes: ["011", "012"],
+    subjects: ["301", "302", "312", "306", "305", "01", "02", "12", "51", "52", "56", "790", "791", "792"],
+    rules: [
+      { length: 11, subStart: 3, subLen: 2 }, // 점(3)-과목(2) 
+      { length: 13, subStart: 0, subLen: 3 }, // 과목(3)-일련(8) 
+      { length: 14, subStart: 3, subLen: 2 }  // 점(3)-과목(2) 
+    ]
+  },
+  {
+    name: "하나은행",
+    codes: ["005", "081"],
+    subjects: ["611", "620", "600", "601", "630", "13", "33", "18", "05", "07", "08", "02", "01", "04"],
+    rules: [
+      { length: 12, subStart: 0, subLen: 3 }, // 차세대: 과목(3) 
+      { length: 11, subStart: 3, subLen: 2 }, // 구: 점(3)-과목(2) 
+      { length: 14, subStart: 13, subLen: 1 } // 외환: 마지막 과목 
+    ]
+  }
+];
---
+++ src/utils/bankMatcher/index.js
@@ -0,0 +1,24 @@
+import { GenerateDetector, AccountNumberDetector } from './detectorClass.js';
+import { BANK_PLUGINS_DATA } from './bankData.js';
+
+// 엔진 인스턴스 생성
+const detectorSystem = new AccountNumberDetector();
+
+// 데이터 기반으로 감지기 등록
+BANK_PLUGINS_DATA.forEach(data => {
+  detectorSystem.addDetector(
+    new GenerateDetector(data.name, data.codes, data.subjects, data.rules)
+  );
+});
+
+/**
+ * 외부에서 사용할 메인 함수
+ * @param {string} accountNumber - 검증할 계좌번호
+ * @returns {Array} score 기반 정렬된 은행 리스트
+ */
+export const detectAccountNumber = (accountNumber) => {
+  return detectorSystem.detectAccountNumber(accountNumber);
+};
+
+// 클래스도 필요한 경우를 위해 별도로 export
+export { GenerateDetector, AccountNumberDetector };
